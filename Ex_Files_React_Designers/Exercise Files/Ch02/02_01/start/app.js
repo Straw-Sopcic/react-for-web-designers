707286291fa20dc5da6e1be5982939c5c534058e
Straw-Sopcic <31935409+Straw-Sopcic@users.noreply.github.com>
2018-05-05 00:29:26 -0500

    another part finished

--- Ex_Files_React_Designers/Exercise Files/Ch02/02_01/start/app.js
+++ Ex_Files_React_Designers/Exercise Files/Ch02/02_01/start/app.js
@@ -1,6 +1,30 @@
 (function() {
   "use strict";
 
+    function SizeSelector(props){
+        function sizeOptions(){
+            var sizes = window.Inventory.allSizes;
+            
+            
+            return sizes.map(function(num){
+                return(
+                    <option value={num} key={num}>
+                        {num}
+                    </option> 
+                );
+            });
+        }
+        
+        
+        return(
+            <div className="field-group">
+                <label htmlFor="size-options">Size:</label>
+                <select name="sizeOptions" id="size-options">
+                    {sizeOptions()}
+                </select>
+            </div>
+              );
+    }
     
     function ProductImage(props){            
         return <img src="../../../assets/red.jpg" alt='Product image'/>;
@@ -11,6 +35,9 @@
             <div className="customizer">
                 <div className='product-image'>
                     <ProductImage />
+                </div>
+                <div className="selectors">
+                    <SizeSelector/>
                 </div>
             </div>
         );
