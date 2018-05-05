a3134f6d5af959df42c6ffbca2065bb7679d20ad
Straw-Sopcic <31935409+Straw-Sopcic@users.noreply.github.com>
2018-05-05 00:12:12 -0500

    made cool changes

--- Ex_Files_React_Designers/Exercise Files/Ch02/02_01/start/app.js
+++ Ex_Files_React_Designers/Exercise Files/Ch02/02_01/start/app.js
@@ -1,27 +1,20 @@
 (function() {
   "use strict";
 
-  // Start here
-   
     
     function ProductImage(props){            
-            return React.createElement('img', {
-            src : "../../../assets/red.jpg",
-            alt : 'Product image'
-        })
+        return <img src="../../../assets/red.jpg" alt='Product image'/>;
     }
     
     function ProductCustomizer(props){
-        return React.createElement(
-            'div',
-            {className: "customizer"},
-            React.createElement('div', {className: 'product-image'}, React.createElement(ProductImage))
+        return(
+            <div className="customizer">
+                <div className='product-image'>
+                    <ProductImage />
+                </div>
+            </div>
         );
     }
     
-    ReactDOM.render(
-        React.createElement(ProductCustomizer),
-        document.getElementById('react-root')
-    );
-
+    ReactDOM.render( <ProductCustomizer/>, document.getElementById('react-root'));
 })();
